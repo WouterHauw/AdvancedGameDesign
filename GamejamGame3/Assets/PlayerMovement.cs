bed71d14882d6073b154a9762f4c2ac90724ba49
Lotta <lottastam@gmail.com>
2017-09-11 15:02:43 +0200

    Begin game 3

---
+++ GamejamGame3/Assets/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Rigidbody2D myRigidBody;
+
+    [SerializeField]
+    private float movementSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        HandleHorizontal(horizontal);
+        HandleVertical(vertical);
+    }
+
+    private void HandleHorizontal(float horizontal)
+    {
+        myRigidBody.velocity = new Vector2(horizontal * movementSpeed, myRigidBody.velocity.y);
+    }
+
+    private void HandleVertical(float vertical)
+    {
+        myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, vertical * movementSpeed);
+    }
+}
