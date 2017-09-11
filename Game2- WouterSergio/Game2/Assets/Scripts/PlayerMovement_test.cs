3dbe639f256890b25f291823d617f6dcd71e5713
Sergio Picauly <sergio.picauly@hva.nl>
2017-09-11 13:46:14 +0200

    Merge remote-tracking branch 'origin/Game2-WouterSergio' into Game2-WouterSergio
    # Conflicts:
    #	Game2- WouterSergio/Game2/Library/CurrentLayout.dwlt
    #	Game2- WouterSergio/Game2/Library/LastSceneManagerSetup.txt
    #	Game2- WouterSergio/Game2/Library/ScriptAssemblies/Assembly-CSharp.dll
    #	Game2- WouterSergio/Game2/Library/assetDatabase3
    #	Game2-
    WouterSergio/Game2/Library/metadata/cd/cd78e4cbb864caa4babc429b2c34bc20
    
    Added new scene and camera

---
+++ Game2- WouterSergio/Game2/Assets/Scripts/PlayerMovement_test.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement_test : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D myRigidbody;
+
+    public bool grounded;
+    public LayerMask whatIsground;
+
+    public Collider2D myCollider;
+
+    void Start()
+    {
+
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
+    }
+
+
+    void Update()
+    {
+        grounded = Physics2D.IsTouchingLayers(myCollider, whatIsground);
+
+        myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+        }
+
+    }
+}
