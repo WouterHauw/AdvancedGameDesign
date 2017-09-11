b929d341b2764451b29d9808dc32dde7503f63ba
snowkillerNL <wouterflapneus@gmail.com>
2017-09-11 12:56:07 +0200

    Added playermovement.

--- Game2- WouterSergio/Game2/Assets/Scripts/PlayerMovement.cs
+++ Game2- WouterSergio/Game2/Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,69 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject ground;
+    public float jumpPower = 2f;
+    public float walkSpeed = 2f;
 
+    private Rigidbody2D rigid;
+    private BoxCollider2D collider;
+    private RaycastHit hit;
+    private float distToTheGround;
+    private Dictionary<KeyCode, Vector3> directions;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
+    }
+
+
+    void Start()
+    {
+        directions = new Dictionary<KeyCode, Vector3>()
+        {
+            
+            {KeyCode.A, Vector3.left}, 
+            {KeyCode.D, Vector3.right},
+            
+        }; 
+    distToTheGround = collider.bounds.extents.y;
+    }
 	// Use this for initialization
-	void Start () {
-		
-	}
+   
+
+
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    if (Physics.Raycast(transform.position, -Vector3.up, distToTheGround+0.1f))
+        //    {
+        //       Jump();
+	    //    }
+	    //}
+	    if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+	    {
+            Jump();
+        }
+        foreach (var direction in directions.Keys)
+	    {
+	        if (Input.GetKey(direction))
+	        {
+	            
+	               this.transform.Translate(directions[direction] * walkSpeed * Time.deltaTime, Space.Self);
+                
+	            
+            }
+	    }
 	}
+
+    void Jump()
+    {
+        rigid.AddForce(transform.up * 9 ,ForceMode2D.Impulse);
+    }
 }
