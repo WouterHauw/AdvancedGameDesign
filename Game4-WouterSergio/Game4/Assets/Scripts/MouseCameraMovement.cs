e97b65679e0c651c2046848ac9b1dfcb6f29235a
snowkillerNL <wouterflapneus@gmail.com>
2017-09-12 10:49:32 +0200

    Edit enivorment plus added camera script.

---
+++ Game4-WouterSergio/Game4/Assets/Scripts/MouseCameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCameraMovement : MonoBehaviour
+{
+
+    public float dragSpeed = 2;
+    private Vector3 dragOrigin;
+
+	void LateUpdate () {
+	    if (Input.GetMouseButtonDown(0))
+	    {
+	        dragOrigin = Input.mousePosition;
+            return;
+	    }
+	    if (!Input.GetMouseButton(0)) return;
+
+	    Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector3 move = new Vector3(pos.x * dragSpeed, pos.y * dragSpeed, 0);
+
+        transform.Translate(move,Space.World);
+	}
+}
