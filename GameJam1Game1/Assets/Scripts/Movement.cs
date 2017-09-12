617705bf36128190475f66363279948dab0c4e99
leeuwk004 <lion.drie@gmail.com>
2017-09-12 09:47:28 +0200

    rip metadata

--- GameJam1Game1/Assets/Scripts/Movement.cs
+++ GameJam1Game1/Assets/Scripts/Movement.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Movement : MonoBehaviour {
 
     private Rigidbody2D myRigidBody;
-
+    public float jumpSpeed;
+    public bool isGrounded = false;
     [SerializeField]
     private float movementSpeed;
 
@@ -14,19 +16,21 @@ public class Movement : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+        //GetComponent<Rigidbody2D>().isKinematic = true;
         myRigidBody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate ()
     {
+        
         float horizontal = Input.GetAxis("Horizontal");
 
 
         HandleMovement(horizontal);
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
         {
-            myRigidBody.velocity = new Vector2(0, jumpHeight);
+            myRigidBody.velocity = new Vector2(0, jumpHeight*jumpSpeed);
         }
     }
 
@@ -35,4 +39,25 @@ public class Movement : MonoBehaviour {
         myRigidBody.velocity = new Vector2(horizontal * movementSpeed, myRigidBody.velocity.y);
        
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "StarTag")
+        {
+            SceneManager.LoadScene("VictoryScene");
+
+        }else if (collision.gameObject.tag == "PlatformTag")
+        {
+            isGrounded = true;
+        }
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
+
+
 }
