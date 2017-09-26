40b9b36d5b23a8feb66f9b4feb672ac5f5bca29a
Sergio Picauly <sergio.picauly@hva.nl>
2017-09-26 12:51:57 +0200

    Created scene + sheep movement

---
+++ Bearly Breathing/Assets/Scripts/DamageHandlerSheep.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageHandlerSheep : MonoBehaviour {
+
+    public float health = 10;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (health == 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
