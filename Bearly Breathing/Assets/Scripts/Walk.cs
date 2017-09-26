ca739b0afb2fd6c5b05b1cdecffb10365b2124a4
Sergio Picauly <sergio.picauly@hva.nl>
2017-09-26 13:41:31 +0200

    Added damage handler + soundeffects

--- Bearly Breathing/Assets/Scripts/Walk.cs
+++ Bearly Breathing/Assets/Scripts/Walk.cs
@@ -2,33 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Walk : StateMachineBehaviour {
+public class Walk : StateMachineBehaviour
+{
 
     GameObject Sheep;
     GameObject[] waypoints;
     int currentWP;
 
-    private AudioSource audio;
+
 
     void Awake()
     {
         waypoints = GameObject.FindGameObjectsWithTag("waypoint");
     }
 
-	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
         Sheep = animator.gameObject;
         currentWP = 0;
-	}
+    }
 
-	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
         if (waypoints.Length == 0) return;
-        if(Vector3.Distance(waypoints[currentWP].transform.position,
+        if (Vector3.Distance(waypoints[currentWP].transform.position,
             Sheep.transform.position) < 3.0f)
         {
             currentWP++;
-            if(currentWP >= waypoints.Length)
+            if (currentWP >= waypoints.Length)
             {
                 currentWP = 0;
             }
@@ -39,10 +42,11 @@ public class Walk : StateMachineBehaviour {
                                    Quaternion.LookRotation(direction),
                                    1.0f * Time.deltaTime);
         Sheep.transform.Translate(0, 0, Time.deltaTime * 2.0f);
-	}
+    }
 
-	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	
-	}
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
 }
