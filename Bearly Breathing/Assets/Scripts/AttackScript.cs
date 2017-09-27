d442fb2757ea5bf23f7f5afb49c47de36870d556
snowkillerNL <wouterflapneus@gmail.com>
2017-09-27 15:18:09 +0200

    Added Bear Attack

---
+++ Bearly Breathing/Assets/Scripts/AttackScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackScript : MonoBehaviour
+{
+
+    public float range = 10;
+    private RaycastHit hit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void KillSheeps()
+    {
+        
+        Vector3 VectorForwards = transform.TransformDirection(Vector3.forward);
+        if (Physics.Raycast(transform.position, VectorForwards, out hit, range))
+        {
+            if (hit.transform.gameObject.tag == "Sheep")
+            {
+                hit.transform.gameObject.SetActive(false);
+            }
+        }
+
+    }
+}
