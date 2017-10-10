2e59eaca57e3b2055ec00c1ba59c4f511aa4a22e
leeuwk004 <lion.drie@gmail.com>
2017-10-10 13:13:28 +0200

    tap to move

--- Bearly Breathing/Assets/Scripts/InputScript.cs
+++ Bearly Breathing/Assets/Scripts/InputScript.cs
@@ -8,21 +8,70 @@ namespace DigitalRubyShared
 
 
 
-        public FingersJoystickScript joyStickScript;
+        //public FingersJoystickScript joyStickScript;
+        private TapGestureRecognizer tapGesture;
+        private SwipeGestureRecognizer swipeGesture;
+
         // Update is called once per frame
         public GameObject player;
-
+        private Camera camera;
+        private GestureTouch t;
         public float speed = 10.0f;
+       // private Vector2 newPosition;
+        Vector3 inWorldPosition;
 
-        public bool MoveJoysticktoGestureStartLocation;
+        private GestureTouch FirstTouch(ICollection<GestureTouch> touches)
+        {
+            foreach(GestureTouch t in touches)
+            {
+                return t;
+            }
+            return new GestureTouch();
+        }
+
+      //  public bool MoveJoysticktoGestureStartLocation;
+
+        public void Start()
+        {
+            CreateTapGesture();
+            camera = Camera.main;
+        }
+
+        public void Update()
+        {
+            player.transform.position = inWorldPosition;
+              //  player.transform.position = Vector3.MoveTowards(player.transform.position, inWorldPosition, speed * Time.deltaTime);
+            
+
+        }
+
+        private void CreateTapGesture()
+        {
+            tapGesture = new TapGestureRecognizer();
+            tapGesture.Updated += TapGestureCallback;
+            FingersScript.Instance.AddGesture(tapGesture);
+        }
+
+        protected void TapGestureCallback(GestureRecognizer gesture, ICollection<GestureTouch> touches)
+        {
+            if (gesture.State == GestureRecognizerState.Ended) {
+                 t = FirstTouch(touches);
+                Debug.Log("the update tap  = " + t.X + ", Y: " + t.Y);
+              
+                inWorldPosition = camera.ScreenToWorldPoint(new Vector3(t.X, t.Y, camera.nearClipPlane));
+                inWorldPosition.y = 0;
+                Debug.Log("Where he actually goes = " + inWorldPosition);
+            }
+         
+        }        
 
         private void Awake()
         {
-            joyStickScript.JoystickExecuted = joystickExecuted;
-            joyStickScript.MoveJoystickToGestureStartLocation = MoveJoysticktoGestureStartLocation;
+           // joyStickScript.JoystickExecuted = joystickExecuted;
+           // joyStickScript.MoveJoystickToGestureStartLocation = MoveJoysticktoGestureStartLocation;
         }
 
-        private void joystickExecuted(FingersJoystickScript script, Vector2 amount)
+        /**private void joystickExecuted(FingersJoystickScript script, Vector2 amount)
         {
             Vector3 pos = player.transform.position;
             pos.x += (amount.x * speed * Time.deltaTime);
@@ -35,6 +84,6 @@ namespace DigitalRubyShared
             
 
 
-        }
+        }**/
     }
 }
---
+++ Bearly Breathing/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,35 @@
+﻿using DigitalRubyShared;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**public class MovementScript :  InputScript
+
+{
+    //  public inputScript;
+
+    public Vector2 newPosition;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        newPosition = InputScript.t;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(Camera.main.ScreenPointToRay(newPosition)))
+    }
+
+    void movement(Vector2 t)
+    {
+        newPosition = t;
+
+
+
+    }
+}**/
