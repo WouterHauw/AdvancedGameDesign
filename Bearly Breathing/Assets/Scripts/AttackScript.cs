7f294ad1299b4990e057e3abd3be9c231b13031a
snowkillerNL <wouterflapneus@gmail.com>
2017-10-10 09:50:43 +0200

    Added Stuff.

--- Bearly Breathing/Assets/Scripts/AttackScript.cs
+++ Bearly Breathing/Assets/Scripts/AttackScript.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class AttackScript : MonoBehaviour
@@ -7,6 +8,10 @@ public class AttackScript : MonoBehaviour
 
     public float range = 10;
     private RaycastHit hit;
+
+    public GameObject blood;
+
+     
 
 	// Use this for initialization
 	void Start () {
@@ -22,6 +27,7 @@ public class AttackScript : MonoBehaviour
             if (hit.transform.gameObject.tag == "Sheep")
             {
                 hit.transform.gameObject.SetActive(false);
+                Instantiate(blood, hit.transform.position, Quaternion.LookRotation(Vector3.up));
             }
         }
 
