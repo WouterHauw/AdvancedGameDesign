d53ea146c2eb38ca0cef35268ee0aa7a8ef6a152
leeuwk004 <lion.drie@gmail.com>
2017-10-10 15:03:04 +0200

    2nd attempt

--- Bearly Breathing/Assets/Scripts/InputScript.cs
+++ Bearly Breathing/Assets/Scripts/InputScript.cs
@@ -14,7 +14,13 @@ namespace DigitalRubyShared
 
         // Update is called once per frame
         public GameObject player;
+        private Ray ray;
         private Camera camera;
+        private float enter ;
+        private Vector3 playerPos;
+        private Vector3 point;
+        private Vector3 targetPos;
+        private Vector2 playerCameraPos;
         private GestureTouch t;
         public float speed = 10.0f;
        // private Vector2 newPosition;
@@ -39,9 +45,13 @@ namespace DigitalRubyShared
 
         public void Update()
         {
-            player.transform.position = inWorldPosition;
-              //  player.transform.position = Vector3.MoveTowards(player.transform.position, inWorldPosition, speed * Time.deltaTime);
-            
+            player.transform.position = Vector3.MoveTowards(player.transform.position, point, speed * Time.deltaTime);
+            Debug.Log("point" + point);
+         //   Debug.Log(player.transform.position);
+            // targetPos = new Vector3(t.X, transform.position.y, t.Y);
+            // player.transform.position = Vector3.MoveTowards(player.transform.position, targetPos, speed * Time.deltaTime);
+            //  player.transform.position = Vector3.MoveTowards(player.transform.position, inWorldPosition, speed * Time.deltaTime);
+
 
         }
 
@@ -55,21 +65,37 @@ namespace DigitalRubyShared
         protected void TapGestureCallback(GestureRecognizer gesture, ICollection<GestureTouch> touches)
         {
             if (gesture.State == GestureRecognizerState.Ended) {
-                 t = FirstTouch(touches);
+                 
+                ray = camera.ScreenPointToRay(new Vector2(t.X, t.Y));
+                if (Physics.Raycast(ray, enter))
+                {
+                    point = ray.GetPoint(enter);
+                  
+                }
+                point.y = transform.position.y;
+
+
+              /**  t = FirstTouch(touches);
                 Debug.Log("the update tap  = " + t.X + ", Y: " + t.Y);
-              
                 inWorldPosition = camera.ScreenToWorldPoint(new Vector3(t.X, t.Y, camera.nearClipPlane));
                 inWorldPosition.y = 0;
-                Debug.Log("Where he actually goes = " + inWorldPosition);
+                playerPos = player.transform.position;
+                playerCameraPos = Camera.main.WorldToScreenPoint(playerPos);
+                Vector2 direction = new Vector2(t.X, t.Y) - playerCameraPos;
+                Vector2.Distance(playerCameraPos, new Vector2(t.X, t.Y));
+              //  Debug.Log("playerPos" + playerPos);
+                Debug.Log("direction = " + direction);
+                Debug.Log("playerCamera = " + playerCameraPos);
+             //   Debug.Log("Where he actually goes = " + inWorldPosition);**/
             }
          
         }        
 
-        private void Awake()
-        {
+      //  private void Awake()
+        //{
            // joyStickScript.JoystickExecuted = joystickExecuted;
            // joyStickScript.MoveJoystickToGestureStartLocation = MoveJoysticktoGestureStartLocation;
-        }
+      //  }
 
         /**private void joystickExecuted(FingersJoystickScript script, Vector2 amount)
         {
