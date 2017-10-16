8579cb9c9a88b797ea0e7df542b169c346f148e9
Sergio Picauly <sergio.picauly@hva.nl>
2017-10-16 13:40:44 +0200

    Added flocking

---
+++ Bearly Breathing/Assets/Scripts/Flocking.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flocking : MonoBehaviour
+{
+
+    public GameObject sheepPrefab;
+    public static int mapSize = 5;
+    static int numSheep = 10;
+    public static GameObject[] allSheep = new GameObject[numSheep];
+
+    public static Vector3 goalPos = Vector3.zero;
+
+    // Use this for initialization
+    void Start()
+    {
+        for(int i = 0; i < numSheep; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-mapSize, mapSize),0,Random.Range(-mapSize, mapSize));
+            allSheep[i] = (GameObject)Instantiate(sheepPrefab, pos, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Random.Range(0,1000) < 50)
+        {
+            goalPos = new Vector3(Random.Range(-mapSize, mapSize), 0, Random.Range(-mapSize, mapSize));
+        }
+    }
+}
