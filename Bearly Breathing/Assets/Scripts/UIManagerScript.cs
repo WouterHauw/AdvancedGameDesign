ea014595f47bfba4b6807b17ffc399bbe2be0e30
snowkillerNL <wouterflapneus@gmail.com>
2017-11-07 11:02:12 +0100

    Edit Canvas ui and edit some code from healthbar.

--- Bearly Breathing/Assets/Scripts/UIManagerScript.cs
+++ Bearly Breathing/Assets/Scripts/UIManagerScript.cs
@@ -1,20 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class UIManagerScript : MonoBehaviour {
     [SerializeField] private PlayerController player;
-    public GameObject text;
+    [SerializeField] private GameObject text;
     [SerializeField] private Slider SliderVar;
 
 	// Use this for initialization
 	void Start () {
-        SliderVar = GetComponent<Slider>();
-        player = player.GetComponent<PlayerController>();
-        text = GameObject.Find("HealthText");
-       
-    }
+	    player = player.GetComponent<PlayerController>();
+
+	}
 	
 	// Update is called once per frame
 	void Update () {
