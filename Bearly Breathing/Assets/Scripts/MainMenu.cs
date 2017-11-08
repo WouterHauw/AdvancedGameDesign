3161da505d97cf7b056e9ed9ee003898919c2da2
Lotta <lottastam@gmail.com>
2017-11-08 16:10:54 +0100

    Added: Main menu scene
    Added: Main menu scene

---
+++ Bearly Breathing/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    [SerializeField] private GameObject toBeAddedText;
+
+    // Use this for initialization
+    void Start () {
+        toBeAddedText.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("2DScene");
+    }
+
+    public void ClickUnfinishedButton()
+    {
+        StartCoroutine("FadeInAndOutText");
+    }
+
+    IEnumerator FadeInAndOutText()
+    {
+        toBeAddedText.SetActive(true);
+        yield return new WaitForSeconds(2);
+        toBeAddedText.SetActive(false);
+    }
+
+
+}
