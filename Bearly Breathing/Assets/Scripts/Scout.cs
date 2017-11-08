50addf72d5e8167d2972ce90722cc052115341b9
Sergio Picauly <sergio.picauly@hva.nl>
2017-11-08 14:27:25 +0100

    Added scout state

---
+++ Bearly Breathing/Assets/Scripts/Scout.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scout : NPCBaseHunter
+{
+
+    GameObject[] waypoints;
+    int currentWP;
+
+    void Awake()
+    {
+        waypoints = GameObject.FindGameObjectsWithTag("waypoint");
+    }
+
+    //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+        currentWP = 0;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        hunter.speed = 15;
+        if (waypoints.Length == 0) return;
+        if (Vector3.Distance(waypoints[currentWP].transform.position,
+            NPC.transform.position) < accuracy)
+        {
+            currentWP = Random.Range(0, waypoints.Length);
+            if (currentWP >= waypoints.Length)
+            {
+                currentWP = 0;
+            }
+        }
+
+        hunter.SetDestination(waypoints[currentWP].transform.position);
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        hunter.speed = 3.5f;
+    }
+}
+
