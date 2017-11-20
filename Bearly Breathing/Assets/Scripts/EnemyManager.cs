20c8a62664d0468e99752928e00a58c0222f57ce
Sergio Picauly <sergio.picauly@hva.nl>
2017-11-20 14:36:28 +0100

    Added spawnpoints

---
+++ Bearly Breathing/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+
+
+
+public class EnemyManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _sheep;
+    [SerializeField] private GameObject _player;
+    [SerializeField] private float spawnTime = 3f;
+    [SerializeField] private Transform[] spawnPoints;
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Spawn()
+    {
+        if (_player == null)
+        {
+            return;
+        }
+
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        Instantiate(_sheep, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
