076fb12b3f132929eca420520fd8288a0baa4d9f
leeuwk004 <lion.drie@gmail.com>
2017-11-21 15:34:58 +0100

    Added days survived and data regarding score

--- Bearly Breathing/Assets/ScoreManager.cs
+++ Bearly Breathing/Assets/ScoreManager.cs
@@ -4,9 +4,11 @@ using UnityEngine.UI;
 public class ScoreManager : MonoBehaviour {
 
     [SerializeField] private PlayerController _player;
+    [SerializeField] private DayNightCycle cycle;
     [SerializeField] private GameObject text;
     [SerializeField] private Slider SliderVar;
-    private int _currentScore, _requiredScore;
+    private int _currentScore, _requiredScore, _survived;
+    
 
     // Use this for initialization
     void Start()
@@ -18,8 +20,10 @@ public class ScoreManager : MonoBehaviour {
 
     private void InitializeVariables()
     {
+        DontDestroyOnLoad(this);
         _player = _player.GetComponent<PlayerController>();
         _requiredScore = 10;
+
     }
 
     // Update is called once per frame
@@ -27,6 +31,7 @@ public class ScoreManager : MonoBehaviour {
     {
         setScoreSlider();
         text.GetComponent<Text>().text = _player._currentScore + "/" + _requiredScore;
+        _survived = cycle.daysSurvived;
     }
 
 
