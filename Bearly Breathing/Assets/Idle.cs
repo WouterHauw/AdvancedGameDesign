dec84ab4c48636c0e6b4bc69aa83748f3cbe71a8
Sergio Picauly <sergio.picauly@hva.nl>
2017-12-11 20:13:17 +0100

    Added idle state

--- Bearly Breathing/Assets/Idle.cs
+++ Bearly Breathing/Assets/Idle.cs
@@ -1,11 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
-public class Flee : SheepFSM
-{
-
+public class Idle : SheepFSM {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
@@ -15,13 +12,12 @@ public class Flee : SheepFSM
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (opponent.transform.position.z > sheep.transform.position.z)
-        {
-            Vector3 runTo = (opponent.transform.position - sheep.transform.position).normalized;
-            //if (NavMesh.SamplePosition(runTo, out navHit, 3.0f, NavMesh.AllAreas))
-            //sheep.destination = navHit.position;
-            //sheep.speed = 5;
-            sheep.destination = runTo;
-        }
+        sheep.isStopped = true;
     }
-}
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        sheep.isStopped = false;
+    }
+
+    }
