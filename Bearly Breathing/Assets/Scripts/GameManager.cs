4291739d8e6b5016a53563d513cd9af44be4b120
Wouter van der Hauw <wouter.van.der.hauw@hva.nl>
2018-01-03 17:15:14 +0100

    Edit Spawn script sheep changed,different sheep spawn.

---
+++ Bearly Breathing/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    public void AdjustScore()
+    {
+        
+    }
+}
