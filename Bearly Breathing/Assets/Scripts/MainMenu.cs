54a14f75dcfa3e840ea0cf6319da1a94d0dbf9b3
Wouter van der Hauw <wouter.van.der.hauw@hva.nl>
2018-01-15 13:35:42 +0100

    Edit Script removed error to added and make some buttons not interacteble.

--- Bearly Breathing/Assets/Scripts/MainMenu.cs
+++ Bearly Breathing/Assets/Scripts/MainMenu.cs
@@ -1,30 +1,11 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private GameObject _toBeAddedText;
 
-    // Use this for initialization
-    private void Start()
-    {
-        _toBeAddedText.SetActive(false);
-    }
     public void StartGame()
     {
         SceneManager.LoadScene("2DScene");
-    }
-
-    public void ClickUnfinishedButton()
-    {
-        StartCoroutine("FadeInAndOutText");
-    }
-
-    private IEnumerator FadeInAndOutText()
-    {
-        _toBeAddedText.SetActive(true);
-        yield return new WaitForSeconds(2);
-        _toBeAddedText.SetActive(false);
     }
 }
