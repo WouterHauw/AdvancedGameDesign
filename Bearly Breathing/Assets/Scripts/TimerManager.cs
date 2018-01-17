22b59e3e4132f8858cc1400cef54d91671275999
Wouter van der Hauw <wouter.van.der.hauw@hva.nl>
2018-01-17 20:25:04 +0100

    Added logic behind the UI clock.

---
+++ Bearly Breathing/Assets/Scripts/TimerManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerManager : MonoBehaviour
+{
+    private Text _hourText;
+    private DayNightCycle _cyclus;
+    private float _oneHour;
+
+	// Use this for initialization
+	private void Start ()
+	{
+	    _cyclus = FindObjectOfType<DayNightCycle>();
+	    _hourText = gameObject.GetComponentInChildren<Text>();
+	    _oneHour = 1.0f / 24f;
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+	{      
+	    int hour = (int)(_cyclus.currentTimeOfDay / _oneHour);
+	    _hourText.text = hour + ":00";
+	}
+}
