3db4cd8534f89fcb8ea1fec3b7800c64163a8a86
spicauly <sergio.picauly@hva.nl>
2018-03-13 20:04:43 +0100

    Added field of view

---
+++ Bearly Breathing/Assets/Editor/FoVEditor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(FieldOfView))]
+public class FoVEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        FieldOfView fov = (FieldOfView)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.viewRadius);
+    }
+}
